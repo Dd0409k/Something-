0f57f6fdf514878c48a5d467a5c280abc11f49c6
Dd0409k <69456775+Dd0409k@users.noreply.github.com>
2025-02-14 23:57:14 +0530

    Update direct.lua
    Test to see if hwid spoof works

--- direct.lua
+++ direct.lua
@@ -1,2 +1,9 @@
-script_key = "HRAWEPWBGEEFjuKmiNtdbRFiWLnzOzDX";
+-- Spoof HWID to match the original one
+local spoofedHWID = "1739547781.5328193"  -- Replace this with the HWID linked to your key
+writefile("Hoho_Intro.txt", spoofedHWID) -- Overwrite with the spoofed HWID
+
+--define ur key 
+script_key = "ezkcfPhJGknkroDLzvlAXZciLBhNFDOH";
+
+-- actual link to the game
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e4aedc7ccd2bacd83555baa884f3d4b1.lua"))()
