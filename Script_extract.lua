8416237b87d344b606edaf2fb026adeac7784838
Dd0409k <69456775+Dd0409k@users.noreply.github.com>
2025-02-15 16:21:55 +0530

    Update Script_extract.lua
    Write file change

--- Script_extract.lua
+++ Script_extract.lua
@@ -1,3 +1,3 @@
 script_key = "HRAWEPWBGEEFjuKmiNtdbRFiWLnzOzDX"; -- store key
 local response = game:HttpGet(https://api.luarmor.net/files/v3/loaders/e4aedc7ccd2bacd83555baa884f3d4b1.lua) --define variable with the script
-writefile("extracted.lua", response.Body) -- write it on a file locally 
+writefile("extracted.txt", response.Body) -- write it on a file locally 
