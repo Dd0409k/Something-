a0af99ee916a882ac1a2b248269075f6f04ba188
Dd0409k <69456775+Dd0409k@users.noreply.github.com>
2025-02-15 20:09:33 +0530

    Update Script_extract.lua(change key + save location)

--- Script_extract.lua
+++ Script_extract.lua
@@ -1,14 +1,14 @@
 -- Spoof HWID
-local spoofedHWID = "1739547781.5328193"  -- Replace with your actual HWID if needed
+local spoofedHWID = "1739608430.6159837"  -- Replace with your actual HWID if needed
 writefile("Hoho_Intro.txt", spoofedHWID)
 
 -- Define your key
-script_key = "HRAWEPWBGEEFjuKmiNtdbRFiWLnzOzDX";
+script_key = "FhsQAnNvjsjQIBpwjFLWrNdQSwNDfrAA";
 
 -- Fetch script
 local script_content = game:HttpGet("https://api.luarmor.net/files/v3/loaders/e4aedc7ccd2bacd83555baa884f3d4b1.lua")
 
 -- Save script to a file
-writefile("extracted.lua", script_content)
+writefile("final_extracted_script.lua", script_content)
 
 print("Script extracted successfully!")
