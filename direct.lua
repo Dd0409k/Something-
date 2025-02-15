751955ad03a0e5534ca76922ca7a0e8c0ffa4583
Dd0409k <69456775+Dd0409k@users.noreply.github.com>
2025-02-15 18:23:26 +0530

    Update direct.lua
    Different key

--- direct.lua
+++ direct.lua
@@ -1,9 +1,9 @@
 -- Spoof HWID to match the original one
-local spoofedHWID = "1739547781.5328193"  -- Replace this with the HWID linked to your key
+local spoofedHWID = "1739626215.590514"  -- Replace this with the HWID linked to your key
 writefile("Hoho_Intro.txt", spoofedHWID) -- Overwrite with the spoofed HWID
 
 --define ur key 
-script_key = "ezkcfPhJGknkroDLzvlAXZciLBhNFDOH";
+script_key = "NhMRWZpgObNPJscWhMacLwoZbgEfUxxj";
 
 -- actual link to the game
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Dd0409k/Something-/refs/heads/main/realdirect.lua"))()
