b76bd1446291cfd4fa980c86cd9ed13b0af64a1d
Dd0409k <69456775+Dd0409k@users.noreply.github.com>
2025-02-16 11:38:12 +0530

    Update BloxFruitsextraction.lua(fix

--- BloxFruitsextraction.lua
+++ BloxFruitsextraction.lua
@@ -3,11 +3,11 @@ local log = "Remote Event & Function Log:\n\n"
 local function logData(name, ...)
     local args = {...}
     local argStr = ""
-    
+
     for i, v in pairs(args) do
         argStr = argStr .. string.format("[%d]: %s\n", i, tostring(v))
     end
-    
+
     log = log .. string.format("🔥 Event Triggered: %s\n%s\n", name, argStr)
     writefile("remote_log.txt", log)
 end
@@ -16,18 +16,18 @@ end
 for _, v in pairs(getgc(true)) do  
     if typeof(v) == "Instance" and v:IsA("RemoteEvent") then  
         v.OnClientEvent:Connect(function(...)  
-            logData("[Client] " .. v.Name, ...)  
+            logData("[Client] RemoteEvent - " .. v.Name, ...)  
         end)
     end  
 end  
 
 -- Hook into all RemoteFunctions
-for _, v in pairs(getgc(true)) do  
-    if typeof(v) == "Instance" and v:IsA("RemoteFunction") then  
-        local oldFunction = v.InvokeServer
+for _, v in pairs(game:GetService("ReplicatedStorage"):GetChildren()) do  
+    if v:IsA("RemoteFunction") then  
+        local oldInvokeServer = v.InvokeServer
         v.InvokeServer = function(self, ...)
-            logData("[Client] " .. v.Name .. " (RemoteFunction)", ...)
-            return oldFunction(self, ...)
+            logData("[Client] RemoteFunction - " .. v.Name, ...)
+            return oldInvokeServer(self, ...)
         end
     end  
 end  
