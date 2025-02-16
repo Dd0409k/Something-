e7ef57f5171e71f31c1e748e3c5bd885c06387aa
Dd0409k <69456775+Dd0409k@users.noreply.github.com>
2025-02-16 11:12:14 +0530

    Update BloxFruitsextraction.lua(store with writefile()

--- BloxFruitsextraction.lua
+++ BloxFruitsextraction.lua
@@ -1,21 +1,10 @@
-print("Extracting LocalScripts...")
+local data = "Extracted LocalScripts:\n\n"
 
 for _, scriptInstance in pairs(getinstances()) do  
     if scriptInstance:IsA("LocalScript") then  
-        print("Found LocalScript: " .. scriptInstance:GetFullName())  
+        data = data .. "Found LocalScript: " .. scriptInstance:GetFullName() .. "\n"
     end  
 end  
 
-print("✅ Done extracting LocalScript names.")
-print("Extracting function sources...")
-
-for _, v in pairs(getgc(true)) do  
-    if type(v) == "function" and not is_synapse_function(v) then  
-        local info = debug.getinfo(v)  
-        if info and info.source then  
-            print("Function from: " .. info.source)  
-        end  
-    end  
-end  
-
-print("✅ Done extracting function sources.")
+writefile("extracted.txt", data)
+print("✅ LocalScripts dumped to extracted.txt")
