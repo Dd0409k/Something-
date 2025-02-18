5f19abdf4809e42bde205ca042ad09a7c2bc0d17
Dd0409k <69456775+Dd0409k@users.noreply.github.com>
2025-02-18 20:11:41 +0530

    Update Kickbypass.lua(fixed kick is not a valid member of player)

--- Kickbypass.lua
+++ Kickbypass.lua
@@ -1,4 +1,12 @@
-local originalKick = game:GetService("Players").LocalPlayer.Kick
-game:GetService("Players").LocalPlayer.Kick = function(...)
-    warn("Blacklist detected, blocking kick...")
+--local originalKick = game:GetService("Players").LocalPlayer.Kick
+--game:GetService("Players").LocalPlayer.Kick = function(...)
+--    warn("Blacklist detected, blocking kick...")
+--end
+local player = game:GetService("Players").LocalPlayer
+
+-- Override Kick function
+if player and player.Kick then
+    player.Kick = function(self, reason)
+        warn("Blocked Kick: " .. tostring(reason))
+    end
 end
